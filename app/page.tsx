381cdc14ad4f371aece729b0317e62e8b717bdb6
kayvin <kayvin4@hotmail.com>
2024-04-13 15:03:36 +0200

    fix: update font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Based place</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <h1>Based Place</h1>
     </main>
   );
 }
