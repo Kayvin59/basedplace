d7fb2205d522394b6d87a9886656cec30d3ee7ae
kayvin <kayvin4@hotmail.com>
2024-04-27 20:46:52 +0200

    fix: extract wagmi config + background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,33 @@
 "use client"
 
-//import type { Metadata } from "next";
+// import type { Metadata } from "next";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createWeb3Modal } from '@web3modal/wagmi/react';
 import { WagmiProvider } from "wagmi";
-import { baseSepolia } from "wagmi/chains";
 
+import { wagmiConfig } from "@/config";
 import { abril_fatface, merriweather } from './font';
 import "./globals.css";
 
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
 
-import { createConfig, http } from "wagmi";
-import { coinbaseWallet, walletConnect } from "wagmi/connectors";
+/* export const metadata: Metadata = {
+  title: "Based Place",
+  description: "Community experiment",
+}; */
 
 // Setup Query Client
 const queryClient = new QueryClient();
 
-// PorjectId for walletConnect
-const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
-console.log('projectId', projectId)
-
-const Web3ModalData = {
-    name: 'Web3Modal',
-    description: 'Web3Modal Test'
-}
 
 // COLOR : #0052FF
-
-export const wagmiConfig = createConfig({
-  chains: [baseSepolia],
-  ssr: true,
-  connectors: [
-    walletConnect({ projectId, Web3ModalData, showQrModal: false } as any),
-    coinbaseWallet({
-      appName: 'Based Place',
-    }),
-  ],
-  transports: {
-    [baseSepolia.id]: http(),
-  },
-})
 
 createWeb3Modal({
   wagmiConfig,
   projectId: projectId || "",
 })
 
-/* 
-export const metadata: Metadata = {
-  title: "Based Place",
-  description: "Community experiment",
-}; */
 
 export default function RootLayout({
   children,
