84dbb96f36aef5f01eb4f99aee56d32ce83c846f
kayvin <kayvin4@hotmail.com>
2024-04-30 16:45:39 +0200

    fix: update acion panel

--- components/ActionPanel.tsx
+++ components/ActionPanel.tsx
@@ -4,22 +4,27 @@ export default function ActionPanel() {
     return (
         <div className="flex justify-evenly mb-16">
             <div className="w-1/3 p-6 bg-white rounded-md text-center border">
-                <h2 className='mb-6 text-2xl font-secondary'>
+                <h1 className='mb-6 text-6xl font-secondary'>
                     Play
-                </h2>
-                <p className='mb-6'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo ducimus corporis aperiam voluptate adipisci quos?
-                </p>
-                <Button className='w-2/3 bg-footer'>CTA</Button>
+                </h1>
+                <div className='mb-6'>
+                    <p>Join the movement!</p>
+                    <p>Bring and build your community.</p>
+                    <p>Unleash your creativity in this unique experiment</p>
+                </div>
+                <Button className='text-lg w-2/3 bg-footer'>Get started</Button>
             </div>
-            <div className="w-1/3 p-6 bg-white rounded-md text-center border">
-                <h2 className='mb-6 text-2xl font-secondary'>
-                    Earn
-                </h2>
-                <p className='mb-6'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo ducimus corporis aperiam voluptate adipisci quos?
-                </p>
-                <Button className='w-2/3 bg-footer'>CTA</Button>
+            <div className="w-1/3 p-6 flex flex-col items-center bg-white rounded-md text-center border">
+                <h1 className='mb-6 text-6xl font-secondary'>
+                    Learn
+                </h1>
+                <div className='mb-6'>
+                    <p>
+                        Newcomers, degens, artists
+                    </p>
+                    <p>Navigate through our docs and blog to learn about our journey </p>
+                </div>
+                <Button className='text-lg w-2/3 bg-footer mt-auto'>Learn more</Button>
             </div>
         </div>
     )
