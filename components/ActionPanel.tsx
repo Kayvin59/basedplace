250e8f089443ee4a2303680f9b65e2ae1166232d
kayvin <kayvin4@hotmail.com>
2024-05-01 01:29:43 +0200

    fix: update mobile panel

--- components/ActionPanel.tsx
+++ components/ActionPanel.tsx
@@ -2,8 +2,8 @@ import { Button } from '@/components/ui/button';
 
 export default function ActionPanel() {
     return (
-        <div className="flex justify-evenly mb-16">
-            <div className="w-1/3 p-6 bg-white rounded-md text-center border">
+        <div className="flex flex-col lg:flex-row items-center lg:items-stretch lg:justify-evenly mb-16">
+            <div className="w-11/12 sm:w-3/4 lg:w-1/3 p-6 mb-6 sm:mb-12 lg:mb-0 bg-white rounded-md text-center border">
                 <h1 className='mb-6 text-6xl font-secondary'>
                     Play
                 </h1>
@@ -14,7 +14,7 @@ export default function ActionPanel() {
                 </div>
                 <Button className='text-lg w-2/3 bg-footer'>Get started</Button>
             </div>
-            <div className="w-1/3 p-6 flex flex-col items-center bg-white rounded-md text-center border">
+            <div className="w-11/12 sm:w-3/4 lg:w-1/3 p-6 flex flex-col bg-white rounded-md text-center border">
                 <h1 className='mb-6 text-6xl font-secondary'>
                     Learn
                 </h1>
@@ -24,7 +24,7 @@ export default function ActionPanel() {
                     </p>
                     <p>Navigate through our docs and blog to learn about our journey </p>
                 </div>
-                <Button className='text-lg w-2/3 bg-footer mt-auto'>Learn more</Button>
+                <Button className='text-lg w-2/3 bg-footer mt-auto lg:self-center'>Learn more</Button>
             </div>
         </div>
     )
