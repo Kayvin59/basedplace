f1d6aa38da178aeda0c623b6eb25e99fc4e0cf2f
kayvin <kayvin4@hotmail.com>
2024-05-11 13:21:28 +0200

    fix: create specific component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,19 @@
 "use client"
 
-import { Button } from '@/components/ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { useWeb3Modal } from '@web3modal/wagmi/react';
-import { Power } from 'lucide-react';
+import AccountDropdown from '@/components/AccountDropdown';
+import ConnectWalletBtn from '@/components/ConnectWalletBtn';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { useAccount, useReadContract, useWriteContract } from 'wagmi';
 import { abi } from '../app/abi';
-import { getEllipsisAddress } from '../lib/utils';
 import logo from '../public/logo.png';
 import mirrorLogo from '../public/mirror.svg';
 import twitterLogo from '../public/twitter.svg';
-import userLogo from '../public/user.svg';
   
 
 export default function Header() {
-    const { open } = useWeb3Modal();
+    
     const { address, isConnected } = useAccount()
     const account = useAccount();
     const { writeContractAsync, isPending } = useWriteContract()
@@ -33,10 +30,6 @@ export default function Header() {
         functionName: 'balanceOf',
         args: ['0x1F58a081369967B2B4c4E2Ad0C44aF016132ef13'] // TODO: replace with account.address
     })
-
-    const handleOpenModal = () => {
-        open();
-    };
 
     const handleMint = async () => {
         if(account.address === undefined) {
@@ -83,44 +76,8 @@ export default function Header() {
             <Link href='https://twitter.com/BasedPlace_' className='mr-4 sm:mr-8'>
                 <Image src={twitterLogo} alt="Twitter Logo" />
             </Link>
-            <div className='mr-4 sm:mr-8'>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className='border border-foreground focus-visible:ring-offset-0 focus-visible:ring-transparent'>
-                            <Image src={userLogo} alt="user logo" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-40">
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuGroup>
-                            <DropdownMenuItem>
-                                <Link href="/#playground">Playground</Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <Link href="/#dashboard">Dashboard</Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem className='hover:cursor-not-allowed'>Profile</DropdownMenuItem>
-                        </DropdownMenuGroup>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuGroup>
-                            <DropdownMenuItem className='hover:cursor-not-allowed'>Docs</DropdownMenuItem>
-                        </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
-            <div>
-                <Button onClick={handleOpenModal} className='ml-auto font-primary font-bold text-l text-white bg-footer hover:bg-foreground'>
-                    {isConnected ? (
-                        <>
-                            <span>{getEllipsisAddress(address)}</span>
-                            <span className='ml-2'>
-                                <Power size={16} />
-                            </span>
-                        </>
-                    ) : ('Connect Wallet')}
-                </Button>
-            </div>
+            <AccountDropdown />
+            <ConnectWalletBtn />
             <span className='separator w-separator absolute h-px -bottom-px bg-border left-1/2 -translate-x-1/2'></span>
         </header>
     );
