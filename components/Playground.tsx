02d5cc62dde02bca235f7fc9971399060c182f79
kayvin <kayvin4@hotmail.com>
2024-05-22 16:03:54 +0200

    feat: add faucet mint + display user balance

--- components/Playground.tsx
+++ components/Playground.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import ColorPicker from "@/components/ColorPicker";
+import { Button } from '@/components/ui/button';
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { colors } from "@/lib/constant";
 import { createClient } from "@/lib/supabase/client";
 import { PixelsProps } from "@/types/index";
 import { useEffect, useState } from "react";
+import { formatUnits } from "viem";
+import { useAccount, useBalance, useWriteContract } from "wagmi";
+import { abi } from "../app/abi";
 
 
 const initialColor: string = "#FFFFFF";
@@ -15,7 +19,12 @@ export default function Playground({ pixels }: { pixels: PixelsProps[]}) {
   const [squareColors, setSquareColors] = useState(pixels.map((pixel) => pixel.color))
   const [selectedIndex, setSelectedIndex] = useState(-1)
   const [openDrawer, setOpenDrawer] = useState(false);
-  const [updatedPixels, setUpdatedPixels] = useState<PixelsProps[]>(pixels); 
+  const [updatedPixels, setUpdatedPixels] = useState<PixelsProps[]>(pixels);
+  const [isMinting, setIsMinting] = useState(false);
+  const [balance, setBalance] = useState<bigint | undefined>('0' as unknown as bigint);
+  
+  const account = useAccount();
+  const { writeContractAsync, isPending } = useWriteContract()
 
   const handleColorClick = (color: string) => {
     setSelectedColor(color);
@@ -77,29 +86,80 @@ export default function Playground({ pixels }: { pixels: PixelsProps[]}) {
     setOpenDrawer(false);
   };
 
+  const handleMint = async () => {
+    if(account.address === undefined) {
+        console.error("Please connect your wallet to mint.");
+        return;
+    }
+
+    try {
+        await writeContractAsync({
+            address: '0x5ddaf93e4E7873B5A34a181d3191742B116aeF9B',
+            abi,
+            functionName: 'mint',
+        },
+        {
+            onSuccess: () => {
+                console.log("Transaction Complete: ");
+                setIsMinting(true)
+            },
+            onError: (error) => {
+                console.error("Error minting: ", error);
+            }
+        })
+    } catch (e) {
+        console.error("Error minting BP token", e);
+    }
+  };
+
+  const result = useBalance({
+    address: account.address,
+    token: '0x5ddaf93e4E7873B5A34a181d3191742B116aeF9B',
+  })
+
+  useEffect(() => {
+    if (result.data) {
+      setBalance(result.data.value);
+    }
+  }, [result.data]);
+
   useEffect(() => {
     setSquareColors(updatedPixels.map((pixel) => pixel.color));
   }, [updatedPixels]);
 
   return (
-    <div className="my-12 mx-auto w-40 h-40">
-      <div className="grid grid-cols-10 grid-rows-10 gap-x-0 gap-y-0 border border-foreground">
-        {squareColors.map((color, index) => (
-          <Drawer key={index} open={openDrawer && selectedIndex === index} onOpenChange={setOpenDrawer}>
-            <DrawerTrigger>
-              <div
-                className="w-4 h-4 cursor-pointer hover:border border-foreground"
-                style={{ backgroundColor: color }}
-                onClick={() => handleSquareClick(index)}
-              ></div>
-            </DrawerTrigger>
-            <DrawerContent>
-              {openDrawer && selectedIndex === index && (
-                <ColorPicker colors={colors} onColorClick={handleColorClick} onConfirm={handleConfirm}/>
-              )}
-            </DrawerContent>
-          </Drawer>
-        ))}
+    <div className="px-6 py-12 flex items-center justify-between gap-6">
+      <div className="flex-1 self-start">
+        <p>To start playing you need some tokens</p>
+        <p>You can mint 5 $BP tokens every 24H</p>
+        <Button className="mt-4 mb-2" onClick={handleMint}>Mint</Button>
+        {isMinting && (
+          <p className="text-green">Minting complete</p>
+        )}
+      </div>
+      <div className="w-40 h-40">
+        <div className="grid grid-cols-10 grid-rows-10 gap-x-0 gap-y-0 border border-foreground">
+          {squareColors.map((color, index) => (
+            <Drawer key={index} open={openDrawer && selectedIndex === index} onOpenChange={setOpenDrawer}>
+              <DrawerTrigger>
+                <div
+                  className="w-4 h-4 cursor-pointer hover:border border-foreground"
+                  style={{ backgroundColor: color }}
+                  onClick={() => handleSquareClick(index)}
+                ></div>
+              </DrawerTrigger>
+              <DrawerContent>
+                {openDrawer && selectedIndex === index && (
+                  <ColorPicker colors={colors} onColorClick={handleColorClick} onConfirm={handleConfirm}/>
+                )}
+              </DrawerContent>
+            </Drawer>
+          ))}
+        </div>
+      </div>
+      <div className="flex-1 self-start text-right">
+        <p>Balance :</p>
+        <p>{balance !== undefined ? formatUnits(balance, 18) : "Loading..."}</p>
       </div>
     </div>
   );
